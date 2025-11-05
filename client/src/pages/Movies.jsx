d19577e109fecb7fac4abee4b00776a1eba8d414
Keenjoo13 <bimalasapkota644@gmail.com>
2025-11-06 00:47:24 +0545

    addds

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,17 +1,16 @@
 import BlurCircle from "../components/BlurCircle";
 import MovieCard from "../components/MovieCard";
-import { useAppContext } from "../context/AppContext";
+
+import { dummyShowsData } from "../assets/assets";
 
 const Movies = () => {
-  const { shows } = useAppContext();
-
-  return shows.length > 0 ? (
+  return dummyShowsData.length > 0 ? (
     <div className="relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]">
       <BlurCircle top="150px" left="0" />
       <BlurCircle bottom="50px" right="50px" />
       <h1 className="text-lg font-medium my-4">Now Showing</h1>
       <div className="flex flex-wrap max-sm:justify-center gap-8">
-        {shows.map((movie) => (
+        {dummyShowsData.map((movie) => (
           <MovieCard movie={movie} key={movie._id} />
         ))}
       </div>
--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
 import Loading from "../components/Loading";
 import { ArrowRightIcon, ClockIcon } from "lucide-react";
